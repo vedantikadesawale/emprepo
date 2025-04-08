30f77d37e978b23a5bde3eaf9f83fb1ac94e5693
vedantika <vedantikadesawale@gmail.com>
2025-04-08 12:20:13 +0530

    added

--- EmpJDBCProject/src/com/hexaware/client/EmpClient.java
+++ EmpJDBCProject/src/com/hexaware/client/EmpClient.java
@@ -4,5 +4,10 @@ public class EmpClient {
 
 	public static void main(String[] args) {
 		System.out.println("WelCome");
+		display();
+	}
+	
+	public static void display() {
+		System.out.println("display");
 	}
 }
