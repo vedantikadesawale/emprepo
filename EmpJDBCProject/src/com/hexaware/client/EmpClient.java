eb08584ccd9e64aa28b78feaf0edcdebafd0e554
Yazhini <yazhiniraja1412@gmail.com>
2025-04-08 12:36:45 +0530

    Updated EmpClient.java locally

--- EmpJDBCProject/src/com/hexaware/client/EmpClient.java
+++ EmpJDBCProject/src/com/hexaware/client/EmpClient.java
@@ -4,11 +4,8 @@ public class EmpClient {
 
 	public static void main(String[] args) {
 		System.out.println("WelCome");
-		display();
-	}
-	
-	public static void display() {
-		System.out.println("display");
+		viewResults();
+		
 	}
 	public static void viewResults() {
 		System.out.println("view results");
