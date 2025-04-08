5d6237823b45a9ffc2e20bdc266e9c36952d18bc
Yazhini <yazhiniraja1412@gmail.com>
2025-04-08 13:18:22 +0530

    Updated Emp.java with empId, name, and salary fields

--- EmpJDBCProject/src/com/hexaware/entity/Emp.java
+++ EmpJDBCProject/src/com/hexaware/entity/Emp.java
@@ -2,4 +2,49 @@ package com.hexaware.entity;
 
 public class Emp {
 
+    private int empId;
+    private String name;
+    private double salary;
+
+    // Constructor
+    public Emp(int empId, String name, double salary) {
+        this.empId = empId;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Default Constructor
+    public Emp() {
+    }
+
+    // Getters and Setters
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Emp [empId=" + empId + ", name=" + name + ", salary=" + salary + "]";
+    }
 }
